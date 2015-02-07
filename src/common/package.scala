40525aff580d0243250f91304be66f023fa498ec
LPTK <lionel.parreaux@gmail.com>
2015-02-07 04:15:17 +0100

    oopsy

---
+++ src/common/package.scala
@@ -0,0 +1,38 @@
+package object common {
+  
+  
+  
+  
+  
+  implicit class TypeSafeEquatable[T](val _value_ : T) extends AnyVal {
+    private def value = _value_
+    def ===[U >: T <: T] (that: U) = value == that
+    def ==~[U <: T] (that: U) = value == that
+    def ~== (that: T) = value == that
+    
+    def =/=[U >: T <: T] (that: U) = value != that
+    def =/~[U <: T] (that: T) = value != that
+    def ~/= (that: T) = value != that
+    
+  }
+  
+  
+  
+  
+  
+  def Id[T] = (a:T) => a
+  
+  
+  
+  
+  
+  
+  
+  def Pokémon[T](value: => T) = try {
+    Left(value)
+  } catch {
+    case e: Throwable => Right(e) // gotta catch 'em all
+  }
+  
+  
+}
